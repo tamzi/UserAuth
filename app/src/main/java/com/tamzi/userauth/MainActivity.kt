602afeda0e44a919786d43a736c5cd9047e93de0
tamzi <tamrefrank@gmail.com>
2021-09-02 05:04:17 +0300

    Initial commit

---
+++ app/src/main/java/com/tamzi/userauth/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.tamzi.userauth
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.tamzi.userauth.ui.theme.UserAuthTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            UserAuthTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    UserAuthTheme {
+        Greeting("User Authentication App")
+}
+}
