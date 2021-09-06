f55539c4b4f1d39cfbebfe1829ec3fba77e71a2d
tamzi <tamrefrank@gmail.com>
2021-09-06 14:56:44 +0300

    Elevation mod

---
+++ app/src/main/java/com/tamzi/userauth/ui/theme/Elevations.kt
@@ -0,0 +1,14 @@
+package com.tamzi.userauth.ui.theme
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+/**
+ * Elevation values that can be themed.
+ */
+@Immutable
+data class Elevations(val card: Dp = 2.dp)
+
+internal val LocalElevations = staticCompositionLocalOf { Elevations() }
