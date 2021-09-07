64571487c637d22a8d071dfd4be7f19e289cf875
tamzi <tamrefrank@gmail.com>
2021-09-07 22:52:09 +0300

    Color pallete arrangement

--- app/src/main/java/com/tamzi/userauth/ui/theme/Color.kt
+++ app/src/main/java/com/tamzi/userauth/ui/theme/Color.kt
@@ -1,22 +1,47 @@
 package com.tamzi.userauth.ui.theme
 
+import androidx.compose.material.Colors
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.compositeOver
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+/*We set out the various theme colours for our shades of dark and light theme
+*
+* */
 
-val white = Color(0xFFFFFFFF)
-val bluish = Color(0xFF5944BE)
+
+/*Theme 1: IndigoTheme:*/
+val indigo = Color(0xFF654D67)
+val lightIndigo = Color(0xFF9D5875)
+val lighterIndigo = Color(0xFFE5ABB2)
 val darkBluish = Color(0xFF312651)
+
+/*Theme 2*/
+val white = Color(0xFFFFFFFF)
+val cream = Color(0xFFF6EDE2)
 val black = Color(0xFF131313)
-val reddish = Color(0xFFFF4343)
-val warmRed = Color(0xFFFF7754)
-val limeGreenish = Color(0xFF4ABB00)
+
+/*Theme 3: MinimalistTheme*/
 val gray = Color(0xFFEFEFEF)
 val deepGray = Color(0xFFE6E4E6)
 val darkGray = Color( 0xFF83829A)
 val lightGray = Color(0xFFF6F6F6)
-val buttonRed = Color(0xFFFF7777CC)
+
+val bluish = Color(0xFF5944BE)
+val lighterBluish = Color(0xFFFF7777CC)
+
+val reddish = Color(0xFFFF4343)
+val warmRed = Color(0xFFFF7754)
+val limeGreenish = Color(0xFF4ABB00)
+
+
+
+/**
+ * Return the fully opaque color that results from compositing [onSurface] atop [surface] with the
+ * given [alpha]. Useful for situations where semi-transparent colors are undesirable.
+ */
+@Composable
+fun Colors.compositedOnSurface(alpha: Float): Color {
+    return onSurface.copy(alpha = alpha).compositeOver(surface)
+}
 
