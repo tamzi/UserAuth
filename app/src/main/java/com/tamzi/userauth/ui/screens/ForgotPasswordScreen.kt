ad99bbb338a050c23c9580605a55d7fd14e82447
tamzi <tamrefrank@gmail.com>
2021-09-23 01:33:10 +0300

    updated project layout

--- app/src/main/java/com/tamzi/userauth/ui/screens/ForgotPasswordScreen.kt
+++ app/src/main/java/com/tamzi/userauth/ui/screens/ForgotPasswordScreen.kt
@@ -1,7 +1,9 @@
+package com.tamzi.userauth.ui.screens
+
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 
 @Composable
-fun FoodAvailableScreen(navController: NavController){
+fun ForgotPasswordScreen(navController: NavController){
 
 }
