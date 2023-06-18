f5954fead31ee54a96288278baa7f5ca1f1be49f
tamzi <tamrefrank@gmail.com>
2023-06-18 18:20:38 +0200

    updated Utils

--- authUI/src/main/java/com/tamzi/userauthui/utils/Elevations.kt
+++ authUI/src/main/java/com/tamzi/userauthui/utils/Elevations.kt
@@ -1,4 +1,16 @@
-package com.tamzi.sky.utils
+package com.tamzi.userauthui.utils
 
-class Elevations {
-}
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+// Elevation values that can be themed.
+
+@Immutable
+data class Elevations(
+    val card: Dp = 2.dp,
+    val Elevation3: Dp = 3.dp,
+)
+
+internal val LocalElevations = staticCompositionLocalOf { Elevations() }
--- authUI/src/main/java/com/tamzi/userauthui/utils/Shape.kt
+++ authUI/src/main/java/com/tamzi/userauthui/utils/Shape.kt
@@ -1,11 +1,16 @@
-package com.tamzi.sky.utils
+package com.tamzi.userauthui.utils
 
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Shapes
 import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    small = RoundedCornerShape(4.dp),
-    medium = RoundedCornerShape(4.dp),
-    large = RoundedCornerShape(0.dp)
+    small = RoundedCornerShape(percent = 66),
+    medium = RoundedCornerShape(size = 0f),
+    large = RoundedCornerShape(
+        topStart = 17.dp,
+        topEnd = 0.dp,
+        bottomEnd = 0.dp,
+        bottomStart = 17.dp
+    )
 )
